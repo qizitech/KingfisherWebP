d70dabffaa136fc61732c722cdeebf1510a402ac
Shaw <cuzval@gmail.com>
2019-02-14 14:56:30 +0800

    Add OptimizedWebPProcessor

--- KingfisherWebP/Classes/WebPProcessor.swift
+++ KingfisherWebP/Classes/WebPProcessor.swift
@@ -31,3 +31,26 @@ public struct WebPProcessor: ImageProcessor {
     }
 }
 
+public struct OptimizedWebPProcessor: ImageProcessor {
+    
+    public let identifier = "com.yeatse.OptimizedWebPProcessor"
+    
+    private let processor: DownsamplingImageProcessor
+    
+    public init(size: CGSize) {
+        processor = DownsamplingImageProcessor(size: size)
+    }
+    
+    public func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> Image? {
+        switch item {
+        case .image(let image):
+            return image
+        case .data(let data):
+            if data.isWebPFormat {
+                return KingfisherWrapper<Image>.image(webpData: data, scale: options.scaleFactor, onlyFirstFrame: options.onlyLoadFirstFrame)
+            } else {
+                return processor.process(item: item, options: options)
+            }
+        }
+    }
+}
